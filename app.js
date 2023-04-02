e95bccfccc5d025f3209a062027ad0e22cef7f61
MehediHasan95 <imehedihasan95@gmail.com>
2023-04-02 12:41:33 +0600

    modal 75% done

--- app.js
+++ app.js
@@ -12,49 +12,92 @@ document.getElementById("see-more").addEventListener("click", function () {
 const displayData = (data) => {
   const container = document.getElementById("data-container");
 
-  for (const e of data) {
-    // console.log(e);
+  data.forEach((elements) => {
     const div = document.createElement("div");
     div.classList.add("p-3", "border", "col-lg-3", "m-3", "p-0");
-    div.innerHTML = `
+    const { id, image, name, published_in, features } = elements;
 
+    div.innerHTML = `
     <div class='main-card'>
-    <img src="${e.image}" alt="" class="img-fluid rounded-3 border" />
+    <img src="${image}" alt="" class="img-fluid rounded-3 border" />
     <h5>Features</h5>
     <ol>
-      <li>${e.features}</li>
+      ${features.map((e) => `<li>${e}</li>`).join("")}
     </ol>
     <hr />
     <div class="card-details d-flex justify-content-between align-items-center w-100 px-3">
       <div>
-        <h5>${e.name}</h5>
-        
-        <p><i class="bi bi-calendar-check-fill"></i> ${e.published_in}</p>
+        <h5>${name}</h5>
+        <p><i class="bi bi-calendar-check-fill"></i> ${published_in}</p>
       </div>
-      <i onclick="loadDetails('${e.id}')" class="bi bi-arrow-right-circle-fill" data-bs-toggle="modal"
+      <i onclick="loadDetails('${id}')" class="bi bi-arrow-right-circle-fill" data-bs-toggle="modal"
       data-bs-target="#staticBackdrop"></i>
     </div>
     </div>
     `;
     container.appendChild(div);
-  }
+  });
 };
 
 const loadDetails = (id) => {
   const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-  console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((detail) => displayDetails(detail.data));
 };
 
 const displayDetails = (details) => {
-  console.log(details);
   const container = document.getElementById("show-details");
+  container.textContent = "";
   const div = document.createElement("div");
+
+  const {
+    description,
+    pricing,
+    image_link,
+    features,
+    integrations,
+    input_output_examples,
+  } = details;
+
+  console.log(features);
+
+  let singleFeatures = [];
+  for (const e in features) {
+    singleFeatures.push(features[e]);
+  }
+
   div.innerHTML = `
-  <div>
-  <p>${details.description}</p>
+  <div class="d-flex justify-content-between">
+  <div class="w-50 m-2 rounded-3 p-2 border custom-bg">
+  <p>${description}</p>
+  <div class="d-flex justify-content-evenly">
+  ${pricing
+    .map(
+      (e) =>
+        `<div class="py-3 text-center w-25 m-2 bg-white rounded-3">${e.price} ${e.plan}</div>`
+    )
+    .join("")}
+  </div>
+  <div class="d-flex justify-content-evenly">
+      <ul>
+      <p class="fw-bold">Features</p>
+       ${singleFeatures.map((e) => `<li>${e.feature_name}</li>`).join("")}
+      </ul>
+      <div>
+      <ul>
+      <p class="fw-bold">Integrations</p>
+        ${integrations.map((e) => `<li>${e}</li>`).join("")}
+      </ul>
+      </div>
+  </div>
+
+  </div>
+  <div class="w-50 m-2 p-2 rounded-3 border">
+  <img src="${image_link[0]}" alt="" class="img-fluid rounded-3 border" />
+  <p class="text-center">${input_output_examples[0].input}</p>
+  <p class="text-center">${input_output_examples[0].output}</p>
+  </div>
   </div>
   `;
   container.appendChild(div);
